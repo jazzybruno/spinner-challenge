c2268ce6d144b6a1a2dfffd6b2841c41fb47f740
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-06-04 00:47:18 +0200

    Added the controls in the spinner class

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -1,7 +1,25 @@
 const Spinner = () => {
     return ( 
-        <div>
-            Hello World
+        <div className="h-[100vh] w-[100%] flex flex-col justify-center items-center">
+            <div>Hello World This is the spinner</div>
+            <div>Hello World this is the daily stuff</div>
+            <div className="bg-[#fff7f7] flex flex-col justify-center items-start pl-5 w-[80%] rounded-md">
+                 <div className="pt-5 ">
+                    <p className="font-bold text-black">CHANCE</p>
+                    <div className="flex gap-1 bg-white p-2 mt-2">
+                        <input type="text" name="" id="" readOnly value={"23%"} className="font-bold text-black"/>
+                        <div className="flex gap-2 ">
+                            <button className="bg-black text-white font-bold p-2 text-center text-sm">Min</button>
+                            <button className="bg-black text-white font-bold p-2 text-center text-sm">10%</button>
+                            <button className="bg-black text-white font-bold p-2 text-center text-sm">25%</button>
+                            <button className="bg-black text-white font-bold p-2 text-center text-sm">50%</button>
+                            <button className="bg-black text-white font-bold p-2 text-center text-sm">Max</button>
+                        </div>
+                    </div>
+                 </div>
+                 <div className="pt-5">Price</div>
+                 <div className="pt-5 pb-5">Control</div>
+            </div>
         </div>
      );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,10 +4,10 @@ import Spinner from "../components/spinner";
 const Main = () => {
   return (
     <div className="w-[100%] h-[100vh] flex flex-row justify-center items-center">
-      <div className="w-[50%] h-[100%] flex flex-col justify-center items-center">
+      <div className="w-[50%] h-[100%] bg-[#d8d8e2] flex flex-col justify-center items-center">
         <Products />
       </div>
-      <div className="w-[50%] h-[100%] bg-red-500 flex flex-col justify-center items-center">
+      <div className="w-[50%] h-[100%] bg-[#d8d8e2] flex flex-col justify-center items-center">
         <Spinner />
       </div>
     </div>
