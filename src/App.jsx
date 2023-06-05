fc2750bebea7161f0caf9cee56cde39762f8145c
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-06-05 09:25:57 +0200

    Debugging deployment errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Main from './pages/main'
+import Main from './pages/Main'
 
 function App() {
   const [count, setCount] = useState(0)
